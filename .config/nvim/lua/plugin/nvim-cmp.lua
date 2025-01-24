033dd5e9924d3438580d0f071d547a370f1f6997
raefer <raeferanderson@outlook.com>
2025-01-24 21:11:45 +0800

    20250124

---
+++ .config/nvim/lua/plugin/nvim-cmp.lua
@@ -0,0 +1,41 @@
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- Autocompletion
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({
+			select = false,
+			behavior = cmp.ConfirmBehavior.Insert,
+		}),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		["C-Space"] = cmp.mapping.complete(), -- in case the auto completion didn't trigger
+		-- Snippet behavior
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	}),
+})
---
+++ .config/nvim/lua/plugin/todo-comments.lua
@@ -0,0 +1,30 @@
+require("todo-comments").setup({
+	config = {
+		signs = true,
+		sign_priority = 8,
+		keywords = {
+			FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+			TODO = { icon = " ", color = "info" },
+			HACK = { icon = " ", color = "warning" },
+			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+			TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+		},
+		colors = {
+			error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+			warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+			info = { "DiagnosticInfo", "#2563EB" },
+			hint = { "DiagnosticHint", "#10B981" },
+			default = { "Identifier", "#7C3AED" },
+			test = { "Identifier", "#FF00FF" },
+		},
+		highlight = {
+			multiline = true,
+			before = "", -- "fg", "bg", ""
+			keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg", ""
+			after = "fg", -- "fg", "bg", ""
+			comments_only = true,
+		},
+	},
+})
